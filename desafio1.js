414c947d4550ffdb94ac7e4ba3bb1c8e83a19a7f
mateus <74462889+teuaraujo@users.noreply.github.com>
2024-05-27 19:02:20 -0300

    Add files via upload

---
+++ desafio1.js
@@ -0,0 +1,27 @@
+let nome  = "Ragnar";
+let level  = 3000;
+
+if(level <=1000){
+    console.log(`O herói de nome ${nome} esta no nivel de Ferro`)
+}
+if(level >1000 && level <= 2000){
+    console.log(`O herói de nome ${nome} esta no nivel de Bronze`)
+}
+if(level >2000 && level <= 5000){
+    console.log(`O herói de nome ${nome} esta no nivel de Prata `)
+}
+if(level >5000 && level <= 7000){
+    console.log(`O herói de nome ${nome} esta no nivel de Ouro`)
+}
+if(level >7000 && level <= 8000){
+    console.log(`O herói de nome ${nome} esta no nivel de Platina`)
+}
+if(level >8000 && level <= 9000){
+    console.log(`O herói de nome ${nome} esta no nivel de Ascendente`)
+}
+if(level >9000 && level <= 10000){
+    console.log(`O herói de nome ${nome} esta no nivel de Imortal`)
+}
+if(level >=10001){
+    console.log(`O herói de nome ${nome} esta no nivel de Radiante `)
+}
